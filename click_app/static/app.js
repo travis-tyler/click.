baff36ef54f5ddb58dccb0379021057dd453853c
travis-tyler <travisatyler@gmail.com>
2020-10-10 18:48:01 -0500

    D3 test

--- click_app/static/app.js
+++ click_app/static/app.js
@@ -7,8 +7,8 @@ function showClicks() {
         console.log(response[0]['total_clicks']);
 
         // Select the click displays
-        d3.select("#user_click_num").text(response[0]['user_clicks']);
-        d3.select("#total_click_num").text(response[0]['total_clicks']);
+        d3.select("#user_click_num").text(response[0]['user_clicks']+1);
+        d3.select("#total_click_num").text(response[0]['total_clicks']+1);
 
     });
 }
