42dd501f07b7497c9b20376b5693fe2771b431d4
travis-tyler <travisatyler@gmail.com>
2020-10-10 10:32:12 -0500

    dropped async

--- click_app/static/app.js
+++ click_app/static/app.js
@@ -7,10 +7,55 @@ function showClicks() {
         console.log(response);
 
         // Select the click displays
-        d3.select(".user_click_num").data(response[0]);
-        d3.select(".total_click_num").data(response[1]);
+        d3.select(".user_click_num").text(response[0]);
+        d3.select(".total_click_num").text(response[1]);
 
     });
 }
+
+let clickButton = d3.select('#clickButton');
+
+
+// Event handler function
+// function showClicks() {
+//     /* data route */
+//     const url = 'https://api.stoplight.io/v1/versions/9WaNJfGpnnQ76opqe/export/oas.json';
+//     d3.json(url).then(function(response) {
+//         console.log(url);
+//         // Prevent the page from refreshing
+//         d3.event.preventDefault();
+//         console.log(response['info']);
+//         d3.select("#user_click_num").text(response['info']);
+//         d3.select("#total_click_num").text(response['info']);
+//     });
+// };
+
+// function showClicks() {
+//     let response = {"cars":50000,"houses":9000}
+
+//     // Prevent the page from refreshing
+//     // d3.event.preventDefault();
+//     console.log(response['cars']);
+//     d3.select("#user_click_num").text(response['cars']);
+//     d3.select("#total_click_num").text(response['houses']);
+// };
+
+// async function showClicks() {  
+//     let url = 'https://api.stoplight.io/v1/versions/9WaNJfGpnnQ76opqe/export/oas.json';
+
+//     console.log(url);
+
+//     let response = await d3.json(url);
+
+//     console.log(url);
+
+
+//     d3.select("#user_click_num").text(response['info']);
+//     d3.select("#total_click_num").text(response['info']);
+
+// };
+
+// // Create event handler
+// clickButton.on('click',showClicks);
 
 showClicks();
